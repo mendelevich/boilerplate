e81e89058243803b3213a73ac7591568e5d9ba99
Eve Mendelevich <ym9382@gmail.com>
2019-01-06 17:38:29 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Logging middleware
+app.use(morgan('dev'));
+
+// Body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static Middleware
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.use('/api', require('./api'));
+
+// Send index.html for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${PORT}`);
+});
